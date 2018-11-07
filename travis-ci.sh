4e1ebcb3e906706ea4bf538a9a7d2926a92d2b20
Akinori ABE <abe_akinori@cyberagent.co.jp>
2018-11-07 14:03:37 +0900

    :sparkles: use Docker arg for Alpine Dockerfile

--- travis-ci.sh
+++ travis-ci.sh
@@ -12,17 +12,6 @@ function yellow_echo() {
     echo -e "\033[33m$@\033[0m"
 }
 
-function check_unpushed_changes() {
-    source ./generate.sh # Update Dockerfile
-
-    if git diff --name-only HEAD | grep "^dockerfiles/$TAG" >/dev/null; then
-	red_echo "[ERROR] Changes for tag $TAG are not uncommited or unpushed."
-	red_echo "        ./generate_all.sh and push the changes"
-	git diff HEAD
-	exit 127
-    fi
-}
-
 ## Print the difference of this pull request into STDOUT.
 function git_diff_pullreq() {
     yellow_echo "Pull request: $TRAVIS_PULL_REQUEST" >&2
@@ -43,48 +32,54 @@ function git_diff_pullreq() {
 }
 
 function build_image() {
-    yellow_echo "[Build] $TAG: Dockerfile is changed."
+    yellow_echo "[Build] Files under $DIRECTORY is changed."
 
-    docker build -t akabe/ocaml:$TAG dockerfiles/$TAG
-    docker history akabe/ocaml:$TAG
+    docker build --rm \
+           --build-arg BASE_IMAGE=$BASE_IMAGE \
+           --build-arg BASE_TAG=$BASE_TAG \
+           --build-arg OCAML_VERSION=$OCAML \
+           -t akabe/ocaml:fresh \
+           "$DIRECTORY"
+    docker history akabe/ocaml:fresh
 }
 
 function test_image() {
-    yellow_echo "[Test] $TAG"
+    yellow_echo "[Test] OCaml version $OCAML on $BASE"
 
-    docker run --rm -v $PWD:$PWD -w $PWD/tests akabe/ocaml:$TAG sh run_test.sh
+    docker run --rm -v $PWD:$PWD -w $PWD/tests akabe/ocaml:fresh sh run_test.sh
 }
 
 function deploy_image() {
     if [[ "$TRAVIS_PULL_REQUEST" == false ]] && [[ "$TRAVIS_BRANCH" == master ]]; then
-	yellow_echo "[Deploy] Deploy image tag $TAG"
-
 	docker login -u $DOCKER_USER -p $DOCKER_PWD
-	docker push akabe/ocaml:$TAG
 
-	for t in $ALIAS; do
-	    yellow_echo "[Deploy] Deploy image tag $TAG as $t"
-	    docker tag akabe/ocaml:$TAG akabe/ocaml:$t
+	for t in ${TAGS[@]}; do
+	    yellow_echo "[Deploy] Deploy image tag $t"
+	    docker tag akabe/ocaml:fresh akabe/ocaml:$t
 	    docker push akabe/ocaml:$t
 	done
     else
-    	yellow_echo "[Deploy] Skip pushing image tag $TAG..."
-
-    	for t in $ALIAS; do
-    	    yellow_echo "[Deploy] Skip pushing image tag $t"
-    	done
+    	yellow_echo "[Deploy] Skip pushing an image."
     fi
 }
 
-check_unpushed_changes
+BASE_IMAGE="$(echo $BASE | grep -o '^[^:]*')"
+BASE_TAG="$(echo $BASE | grep -o '[^:]*$')"
+DIRECTORY=''
+
+if [ "$BASE_IMAGE" == alpine ]; then
+    DIRECTORY=dockerfiles/alpine
+else
+    red_echo "[Error] Unknown base image $BASE"
+fi
 
 git_diff_pullreq > pullreq.diff
 cat pullreq.diff
 
-if grep "^+++ b/dockerfiles/$TAG/Dockerfile" pullreq.diff >/dev/null; then
+if grep "^+++ b/$DIRECTORY" pullreq.diff >/dev/null; then
     build_image
     test_image
     deploy_image
 else
-    green_echo "[ OK ] $TAG: Dockerfile is not changed."
+    green_echo "[ OK ] Files under $DIRECTORY is not changed."
 fi

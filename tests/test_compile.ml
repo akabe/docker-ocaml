0c4b0f92aa6923df3348caffd8a82b7a95587fb0
Akinori ABE <aabe.65535@gmail.com>
2019-12-18 14:31:16 +0900

    support 4.08 and 4.09 only on alpine

--- tests/test_compile.ml
+++
@@ -1,8 +0,0 @@
-(** Tests for ocaml commands *)
-
-open Graphics (* Check existence of Graphics module. *)
-
-let _ = Graphics.red
-
-let () =
-  print_endline "1dfde4ba-3fd3-417b-b8c2-002092f1e42e"
